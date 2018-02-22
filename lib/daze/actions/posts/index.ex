0f09ed223c41868fd68809dad93748d119e3a261
German Velasco <germsvel@gmail.com>
2018-02-22 12:49:13 -0500

    Update EEx to use SmartEngine
    Update EEx to use assigns and SmartEngine

--- lib/daze/actions/posts/index.ex
+++ lib/daze/actions/posts/index.ex
@@ -4,9 +4,9 @@ defmodule Daze.Actions.Posts.Index do
 
   def init(opts), do: opts
 
-  EEx.function_from_file(:def, :index, "lib/daze/templates/posts/index.html.eex", [:adjective])
+  EEx.function_from_file(:def, :index, "lib/daze/templates/posts/index.html.eex", [:assigns])
 
   def call(conn, _opts) do
-    send_resp(conn, 200, index("coolest"))
+    send_resp(conn, 200, index(adjective1: "coolest", adjective2: "fastest"))
   end
 end
