416804a74db83c7745099538da7e0e149ccc44ab
German Velasco <germsvel@gmail.com>
2018-02-22 12:47:26 -0500

    Introduce EEx without assigns
    Intro EEx but use positional argumments first

--- lib/daze/actions/posts/index.ex
+++ lib/daze/actions/posts/index.ex
@@ -1,9 +1,12 @@
 defmodule Daze.Actions.Posts.Index do
   import Plug.Conn
+  require EEx
 
   def init(opts), do: opts
 
+  EEx.function_from_file(:def, :index, "lib/daze/templates/posts/index.html.eex", [:adjective])
+
   def call(conn, _opts) do
-    send_resp(conn, 200, "Plugs all the way down!")
+    send_resp(conn, 200, index("coolest"))
   end
 end
