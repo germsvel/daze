9fb672174725bd73c18116a9313d746c22945223
German Velasco <germsvel@gmail.com>
2018-02-22 12:34:35 -0500

    Add Plug.ErrorHandler
    Adds Plug.ErroHandler to be able to deal with errors in production.
    Worth noting is the fact that if used with Plug.Debugger, it should go
    after it (or else Debugger would not work).
    
    `handle_error/2` needs to take in a `conn` and a map with the kind of
    error (e.g. :throw), the reason for the error which could be an
    exception or a different term, and the stacktrace.

--- lib/daze/router.ex
+++ lib/daze/router.ex
@@ -5,11 +5,19 @@ defmodule Daze.Router do
     use Plug.Debugger, style: [primary: "#c0392b", accent: "#41B577"]
   end
 
+  use Plug.ErrorHandler
+
   plug(Plug.Logger)
   plug(:match)
   plug(:dispatch)
 
   get "/" do
     send_resp(conn, 200, "We have a router!")
+  end
+
+  def handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack}) do
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_resp(conn.status, "<h2>We're terribly sorry. Something went wrong</h2>")
   end
 end
